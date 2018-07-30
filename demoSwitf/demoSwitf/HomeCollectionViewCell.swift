671aa8b57b3df447c51be8e5bb3c939bcd4ffd92
sundusk <zhuyue346@gmail.com>
2018-07-30 18:18:56 +0800

    添加自定义cell

---
+++ demoSwitf/demoSwitf/HomeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HomeCollectionViewCell.swift
+//  demoSwitf
+//
+//  Created by zhongchuren on 2018/7/30.
+//  Copyright © 2018年 sundusk. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+}
